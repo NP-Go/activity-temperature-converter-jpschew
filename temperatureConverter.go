d53ba8c1370278a4f7bb63b725fc963f5191d9ce
jpschew <jps.chew@gmail.com>
2022-03-29 23:30:57 +0800

    added code to convert temperature from different formats

--- temperatureConverter.go
+++ temperatureConverter.go
@@ -1,24 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func convertKelvin(temperatureInput float64) (float64, float64) {
 	//Insert your code here
-
+	resultFahrenheit := (9.0/5.0)*temperatureInput - 459.67
+	resultCelsius := (5.0 / 9.0) * (resultFahrenheit - 32)
 	//Do not remove this line
 	return resultFahrenheit, resultCelsius
 }
 
 func convertCelsius(temperatureInput float64) (float64, float64) {
 	//Insert your code here
-
+	resultFahrenheit := (9.0/5.0)*temperatureInput + 32
+	resultKelvin := (5.0 / 9.0) * (resultFahrenheit + 459.67)
 	//Do not remove this line
 	return resultFahrenheit, resultKelvin
 }
 
 func convertFahrenheit(temperatureInput float64) (float64, float64) {
 	//Insert your code here
-
+	resultKelvin := (5.0 / 9.0) * (temperatureInput + 459.67)
+	resultCelsius := (5.0 / 9.0) * (temperatureInput - 32)
 	//Do not remove this line
 	return resultKelvin, resultCelsius
 }
@@ -33,17 +39,19 @@ func main() {
 
 	if temperatureChoice == 1 {
 		//Insert Code here
-
+		resultFahrenheit, resultCelsius := convertKelvin(temperatureInput)
+		resultFahrenheit = math.Round(resultFahrenheit*100) / 100
+		resultCelsius = math.Round(resultCelsius*100) / 100
 		//DO not remove this
 		fmt.Println("Fahrenheit: ", resultFahrenheit, " Celsius: ", resultCelsius)
 	} else if temperatureChoice == 2 {
 		//Insert Code here
-
+		resultFahrenheit, resultKelvin := convertCelsius(temperatureInput)
 		//DO not remove this
 		fmt.Println("Fahrenheit: ", resultFahrenheit, " Kelvin: ", resultKelvin)
 	} else if temperatureChoice == 3 {
 		//Insert Code here
-
+		resultKelvin, resultCelsius := convertFahrenheit(temperatureInput)
 		//DO not remove this
 		fmt.Println("Kelvin: ", resultKelvin, " Celsius: ", resultCelsius)
 	} else {
